7f31c5752d5f7873e99c5eeec8afb18daf8b9a06
NatanaelBarbosa <natanaellean2@gmail.com>
2026-08-03 19:20:04 -0300

    Atualizar o cidades.c

--- lab1/ex1/cidades.c
+++ lab1/ex1/cidades.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int calcular(cidade) {
+int calcular(int cont, char * cidade) {
 	// caractere, contador
 	// passo base: c[0]
 	// passo recursivo: c[x] = "\0"
-	
+ char c = cidade[cont];
 
 	if (c != "\0") {
-		cont = calcular(cidade)+1;
+	    calcular(cont+1, cidade)
 	}
 	
 	return cont;
@@ -19,7 +19,7 @@ int main() {
 
 	scanf("%[^\n]", cidade); getchar();
 	while (strcmp(cidade, "FIM") != 0) { 
-		printf("%ld\n", strlen(cidade));
+		printf("%ld\n", calcular(0, cidade));
 		scanf("%[^\n]", cidade); getchar();
 	}
 	return 0;
